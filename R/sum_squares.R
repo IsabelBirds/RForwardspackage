3896cab35137ce1e5aaa329e4893f71a42f8a27c
Isabel Birds <is.birds@btinternet.com>
2020-01-07 12:19:33 +0000

    Added manual to sum_squares

--- R/sum_squares.R
+++ R/sum_squares.R
@@ -1,3 +1,10 @@
-sum_squares <- function(v){
-  sum((v - mean(v))^2)
-}
+#' Calculate the sums of squares for a sample
+#'
+#'returns the sum of the squared deviations from mean, SS(x) of a numberic vector
+#'
+#' @param v numeric vector
+#'
+#' @return
+#' @export
+#'
+#' @examples sum_squares(rnorm(50))
