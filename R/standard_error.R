6fe83fa0d3b84dcccc6b3c87cd4ed2cfd72c6a97
Isabel Birds <is.birds@btinternet.com>
2020-01-07 13:17:13 +0000

    Added standard error function

---
+++ R/standard_error.R
@@ -0,0 +1,11 @@
+#' Calculate the standard error of a sample
+#'
+#' @param x numeric vector
+#' @param n numeric vector
+#'
+#' @return
+#' @export
+#'
+#' @examples
+standard_error <- function(x,n){
+  x/sqrt(n)}
